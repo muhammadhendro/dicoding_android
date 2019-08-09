628069aeab7ae5d4928d2e56e11f6e0602c49290
Muhammad Hendro <40570379+muhammadhendro@users.noreply.github.com>
2019-08-08 18:07:44 -0700

    Initial commit

---
+++ BarVolume/README.md
@@ -0,0 +1,16 @@
+# BarVolume
+Merupakan salah satu hasil latihan dari Kelas Academy Dicoding.
+
+## Intro
+Latihan dari kelas Belajar Membuat Aplikasi Android Untuk Pemula.
+
+Menghitung volume dari bangun ruang, pßanjang x lebar x tinggi.
+
+## IDE
+
+```
+Android Studio 3.1
+```
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ MyViewAndViews/app/src/main/java/com/misagani/myviewandviews/MainActivity.java
@@ -0,0 +1,17 @@
+package com.misagani.myviewandviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getSupportActionBar().setTitle("Google Pixel");
+    }
+}
